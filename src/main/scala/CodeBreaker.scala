ce0541287ba5a8d76cbd9cae6954b98d5d3d2df2
Niklas Gustavsson <niklas@protocol7.com>
2011-06-10 11:38:33 +0200

    Using pattern matching rather than the ugly tuple accessors

--- src/main/scala/CodeBreaker.scala
+++ src/main/scala/CodeBreaker.scala
@@ -5,7 +5,7 @@ class CodeBreaker(secret: String) {
 
   def ?(guess: String) = {
     // produces two list of tuples, one with position matches, and the rest
-    val (ps, others) = guess.split(" ") zip secrets partition (t => t._1 == t._2 )
+    val (ps, others) = guess.split(" ") zip secrets partition {case (g,s) => g == s }
 
     // splits the tuples into two separate lists
     val (sr, gr) = others unzip
