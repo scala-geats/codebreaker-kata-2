de02f64ee303a8ac750c3f8996b3954e556fdfc1
Niklas Gustavsson <niklas@protocol7.com>
2011-06-12 16:32:17 +0200

    Mixed up the guesses and secrets

--- src/main/scala/CodeBreaker.scala
+++ src/main/scala/CodeBreaker.scala
@@ -7,8 +7,8 @@ class CodeBreaker(secret: String) {
     // produces two list of tuples, one with position matches, and the rest
     val (ps, others) = guess.split(" ") zip secrets partition {case (g,s) => g == s }
 
-    // splits the tuples into two separate lists
-    val (sr, gr) = others unzip
+    // splits the remaining list of tuples into two separate lists again
+    val (gr, sr) = others unzip
 
     // intersects (returns only those elements which exists in both lists)
     // then maps to "p" or "m"
